be086b657af6ff1c090ad3e9ad27282e8b1e06b1
manupolo <manpolten@gmail.com>
2017-05-12 13:45:04 +0200

    12/05/17 13:44

--- insertar.php
+++ insertar.php
@@ -1,29 +1,33 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "incidencias";
 
+$cif = $_POST["cif"];
+$nom = $_POST["nom"];
+$dir = $_POST["dir"];
+$img = $_FILES["img"]["name"];
+$ruta = $_FILES["img"]["tmp_name"];
+$destino = "fotos/".$img;
+copy($ruta, $destino);
 
-$nom= $_POST["nombre"];
-$desc= $_POST["descripcion"];
-$foto= addslashes(file_get_contents($_FILES['foto']['tmp_name']));
-$dir= $_POST["direccion"];
-$fecha = date("Y/m/d");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-$conexion = mysqli_connect("localhost","root","","incidencias");
-$insertar = "INSERT INTO incidencia (nombre, descripcion, foto, localizacion, fechaSuceso) VALUES ($nom, $desc, $foto, $dir, $fecha)";
-$resultado = mysqli_query($conexion, $insertar);
+$insertar = "INSERT INTO empresa (cif, nombre, direccion, logo) VALUES ('$cif', '$nom', '$dir', '$img')";
 
-if(resultado){
-	echo "<script language='javascript'>"; 
-	echo "alert('Se ha insertado correctamente.')";
-	echo 'alert("El nombre es '.$nom.'")';
-	echo 'alert("La descripcion es '.$desc.'")';
-	echo 'alert("La direccion es '.$dir.'")';
-	echo 'alert("La foto es '.$foto.'")';
-	echo 'alert("La fecha es '.$fecha.'")';
-	echo "</script>";  
-}else{
-	echo "<script language='javascript'>"; 
-	echo "alert('Ocurrio un error al insertar.')"; 
-	echo "</script>";  
+if ($conn->query($insertar)) {
+    echo "New record created successfully";
+    echo '<p> nombre es: '.$nom.'</p>';
+} else {
+    echo "Error: " . $insertar . "<br>" . $conn->error;
+    echo '<p> nombre es: '.$nom.'</p>';
 }
 
+$conn->close();
 ?>
