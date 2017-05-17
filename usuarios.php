2230c56dfb7d47036afd7e9d1e9614b1393dfc2a
malekith94 <erpejime@gmail.com>
2017-05-17 14:30:25 +0200

    17-05-2017 14:30

--- usuarios.php
+++ usuarios.php
@@ -71,15 +71,15 @@
 		<ul class="collapsible popout" data-collapsible="accordion">
 			<li>
 				<div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 			<li>
 				<div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 			<li>
 				<div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 		</ul>
 		</div>
@@ -90,15 +90,15 @@
 		<ul class="collapsible popout" data-collapsible="accordion">
 			<li>
 				<div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 			<li>
 				<div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 			<li>
 				<div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 		</ul>
 		</div>
@@ -109,15 +109,16 @@
 		<ul class="collapsible popout" data-collapsible="accordion">
 			<li>
 				<div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span> <a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a>
+                </div>
 			</li>
 			<li>
 				<div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 			<li>
 				<div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span></div>
+				<div class="collapsible-body prueba"><span>Lorem ipsum dolor sit amet.</span><a href="#!" class="secondary-content"><i class="material-icons">mode edit</i><i class="material-icons">delete</i></a></div>
 			</li>
 		</ul>
 		</div>
