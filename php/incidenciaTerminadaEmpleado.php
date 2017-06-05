4899f1b8bd6202e278880facb30b5d6d841bf230
malekith94 <erpejime@gmail.com>
2017-06-05 19:34:34 +0200

    19:34 05/06/2017

--- php/incidenciaTerminadaEmpleado.php
+++ php/incidenciaTerminadaEmpleado.php
@@ -1,9 +1,11 @@
 <?php
 
-include("conexion.php");
+include("../conexion.php");
 
 $fecha = date('Y-m-d');
 
 $actualizar = "UPDATE incidencia SET estado=2, fechaResolucion='$fecha'"; 
+
+echo "<html><body><script> window.location = '../indexEmpleado.php' </script></body></html>";
 
 ?>
